68a9f7762b269a5d057cd01700ed95cad293bbab
matt <mdgraser@gmail.com>
2015-02-02 15:42:19 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-HighScore.create!({name: 'Matt', score: 9001})
+HighScore.create!({name: 'Mark Doss', score: 91})
+HighScore.create!({name: 'Buttguy', score: 9001})
+HighScore.create!({name: 'Ryan', score: -5})
+HighScore.create!({name: 'Benderson', score: 87})
+HighScore.create!({name: 'Ned', score: 143})
+HighScore.create!({name: 'Curie', score: 19})
+HighScore.create!({name: 'Markov', score: 116})
